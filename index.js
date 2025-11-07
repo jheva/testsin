4be44f93961d34009d86affd68f867a32d1ede5b
jheva <52994190+jheva@users.noreply.github.com>
2025-11-07 12:55:56 +0800

    Change default CFPORT from 443 to 8443

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域�
 const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
-const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
+const CFPORT = process.env.CFPORT || 8443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'testsin';                        // 节点名称
 
 // 创建运行文件夹
